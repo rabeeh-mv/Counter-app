e4bc349d28515633c6f9ee8077c49e2a4ab3a4d6
Rabeeh mv <rabeehmv333@gmail.com>
2024-10-31 11:46:54 -0700

    therd

--- src/main/Home.js
+++ src/main/Home.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import './home.css'
 function Home() {
     const [count , setCount] = useState(0)
+    
+    const [visible, setVisible] = useState("none")
     const increment = ()=>{
         setCount(count + 1)
     }
@@ -12,17 +14,34 @@ function Home() {
     const reset =()=>{
       setCount(0)
     }
+    const visiblity =()=>{
+      setVisible("block")
+    }
+
   return (
     <>
     <div className='container'>
       <h2>Counter App</h2>
-      <div className='count-number' onClick={increment}>
+      <div className='button' onClick={increment}>
         <h1 className='number'>{count}</h1>
       </div>
       </div>
       <div className='edit-part'>
         <div onClick={reset} className='edit-box'><h4>Reset</h4></div>
-        <div className='edit-box'><h4>Set</h4></div>
+        <div className='edit-box' onChange={visiblity}><h4>Set</h4></div>
+      </div>
+      <div className='set-box'style={{display:visible}}>
+        <div  className='selction-wrapper'>
+          <div className='selections'>
+            <input type="radio" name='set'  id='down'/>
+            <label htmlFor="down">Down</label>
+          </div>
+          <div className='selections'>
+            <input type="radio" id='up' name='set'/>
+            <label htmlFor="up">Up</label>
+          </div>
+        </div>
+          <input className='input-nb' type="number" />
       </div>
     </>
   )
